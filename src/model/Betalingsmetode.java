95c738292ea1e0ac96ea8696ccc390c900bf24a9
Kristoffer <voldsen@outlook.dk>
2022-10-28 09:41:00 +0200

    Hej folk

--- src/model/Betalingsmetode.java
+++ src/model/Betalingsmetode.java
@@ -1,4 +1,16 @@
 package model;
 
 public class Betalingsmetode {
+    private String navn;
+    private Ordre ordre;
+    private Salg salg;
+
+    public Betalingsmetode(String navn){
+        this.navn = navn;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
 }
+
--- src/model/Salg.java
+++ src/model/Salg.java
@@ -1,4 +1,31 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Salg {
+    private double pris;
+    private LocalDate dato;
+    private Betalingsmetode betalingsmetode;
+    private Ordre ordre;
+
+    public Salg (double pris, LocalDate dato){
+        this.pris = pris;
+        this.dato = dato;
+    }
+
+    public double getPris() {
+        return pris;
+    }
+
+    public LocalDate getDato() {
+        return dato;
+    }
+
+    public Betalingsmetode getBetalingsmetode() {
+        return betalingsmetode;
+    }
+
+    public Ordre getOrdre() {
+        return ordre;
+    }
 }
