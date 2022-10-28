4c879605d3a37d59924cc388286b4ebdb0c9bce6
Damse96 <eaaankd@students.eaaa.dk>
2022-10-28 09:34:45 +0200

    yes

--- src/model/Kunde.java
+++ src/model/Kunde.java
@@ -1,4 +1,39 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Kunde {
+    private String navn;
+    private int alder;
+    private int tlfNr;
+    private String email;
+    ArrayList<Ordre> ordres;
+
+    public Kunde(String navn, int alder, int tlfNr, String email, ArrayList<Ordre> ordres) {
+        this.navn = navn;
+        this.alder = alder;
+        this.tlfNr = tlfNr;
+        this.email = email;
+        this.ordres = ordres;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public int getAlder() {
+        return alder;
+    }
+
+    public int getTlfNr() {
+        return tlfNr;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public ArrayList<Ordre> getOrdres() {
+        return ordres;
+    }
 }
