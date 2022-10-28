4ba979796545fdda779f0947c4f2b12952db1ee8
Henrik <eaahve@students.eaaa.dk>
2022-10-28 09:36:22 +0200

    Rundvisning og Ordrelinie

--- src/model/OrdreLinje.java
+++ src/model/OrdreLinje.java
@@ -1,4 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+
 public class OrdreLinje {
+    private int antal;
+    private ArrayList<Produkt> produkter;
+
+    public OrdreLinje(int antal) {
+        this.antal = antal;
+    }
+
+    public int getAntal() {
+        return antal;
+    }
+
+    public void setAntal(int antal) {
+        this.antal = antal;
+    }
+
+    public ArrayList<Produkt> getProdukter() {
+        return produkter;
+    }
+
+    public void setProdukter(ArrayList<Produkt> produkter) {
+        this.produkter = produkter;
+    }
 }
--- src/model/Rundvisning.java
+++ src/model/Rundvisning.java
@@ -1,4 +1,39 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Rundvisning {
+    private LocalDateTime dato;
+    private int antalDeltagelser;
+    private String navn;
+
+    public Rundvisning(LocalDateTime dato, int antalDeltagelser, String navn) {
+        this.dato = dato;
+        this.antalDeltagelser = antalDeltagelser;
+        this.navn = navn;
+    }
+
+    public LocalDateTime getDato() {
+        return dato;
+    }
+
+    public void setDato(LocalDateTime dato) {
+        this.dato = dato;
+    }
+
+    public int getAntalDeltagelser() {
+        return antalDeltagelser;
+    }
+
+    public void setAntalDeltagelser(int antalDeltagelser) {
+        this.antalDeltagelser = antalDeltagelser;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
 }
