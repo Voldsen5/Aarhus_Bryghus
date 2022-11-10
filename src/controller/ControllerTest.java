5b23a776ad8fe4a4a1da18000a2899f739f1e349
bakkigit <bakki1@hotmail.com>
2022-11-10 21:59:07 +0100

    30/10/2022

--- src/controller/ControllerTest.java
+++ src/controller/ControllerTest.java
@@ -37,80 +37,186 @@ class ControllerTest {
     void procentRabat1() {
         //Arrange
         Produkt test = Controller.createProdukt("test");
-        Controller.createOrdreLinje(test, 2);
-        Controller.createPris(500, null,test);
+        Controller.createOrdreLinje(test, 1);
+        Controller.createPris(100, null,test);
 
 
         //Act
-        double actualResult = Controller.procentRabat(50);
+        double actualResult = Controller.procentRabat(0);
 
         //Assert
-        assertEquals(500, actualResult);
+        assertEquals(100, actualResult);
 
     }
     @Test
     void procentRabat2() {
         //Arrange
         Produkt test = Controller.createProdukt("test");
-        Controller.createOrdreLinje(test, 3);
-        Controller.createPris(600, null,test);
+        Controller.createOrdreLinje(test, 1);
+        Controller.createPris(100, null,test);
+
+
+        //Act
+        double actualResult = Controller.procentRabat(1);
+
+        //Assert
+        assertEquals(99, actualResult);
+
+    }
+
+    @Test
+    void procentRabat3() {
+        //Arrange
+        Produkt test = Controller.createProdukt("test");
+        Controller.createOrdreLinje(test, 1);
+        Controller.createPris(100, null,test);
 
 
         //Act
         double actualResult = Controller.procentRabat(50);
 
         //Assert
-        assertEquals(900, actualResult);
+        assertEquals(50, actualResult);
 
     }
+
+    @Test
+    void procentRabat4() {
+        //Arrange
+        Produkt test = Controller.createProdukt("test");
+        Controller.createOrdreLinje(test, 1);
+        Controller.createPris(100, null,test);
+
+
+        //Act
+        double actualResult = Controller.procentRabat(99);
+
+        //Assert
+        assertEquals(1, actualResult);
+
+    }
+
+
+    @Test
+    void procentRabat5() {
+        //Arrange
+        Produkt test = Controller.createProdukt("test");
+        Controller.createOrdreLinje(test, 1);
+        Controller.createPris(100, null,test);
+
+
+        //Act
+        double actualResult = Controller.procentRabat(100);
+
+        //Assert
+        assertEquals(0, actualResult);
+
+    }
+
+    @Test
+    void procentRabat6() {
+        //Arrange
+        Produkt test = Controller.createProdukt("test");
+        Controller.createOrdreLinje(test, 1);
+        Controller.createPris(100, null,test);
+
+
+        //Act
+        double actualResult = Controller.procentRabat(101);
+
+        //Assert
+        assertEquals(-1, actualResult);
+
+    }
+
+//--------------------------------------------------------------------------------------------------------------------------
+
     @Test
     void samletPantPris() {
         //Arrange
-        Produkt s1 = Controller.createProdukt("bakki");
-        Produkt s2 = Controller.createProdukt("bakki");
-        Produkt s3 = Controller.createProdukt("bakki");
-        s1.setPantPris(100);
-        s2.setPantPris(50);
-        s3.setPantPris(100);
+        Produkt s1 = Controller.createProdukt("Fustage");
+        s1.setPantPris(200);
 
         //Act
         double actualResult = Controller.samletPantPris();
 
         //Assert
-        assertEquals(250, actualResult);
+        assertEquals(200, actualResult);
     }
 
     @Test
     void samletPantPris2() {
         //Arrange
-        Produkt s1 = Controller.createProdukt("bakki");
-        Produkt s2 = Controller.createProdukt("bakki");
-        Produkt s3 = Controller.createProdukt("bakki");
+        Produkt s1 = Controller.createProdukt("Fustage");
+        Produkt s2 = Controller.createProdukt("Fustage");
+        Produkt s3 = Controller.createProdukt("Fustage");
+        Produkt s4 = Controller.createProdukt("Fustage");
+        Produkt s5 = Controller.createProdukt("Fustage");
         s1.setPantPris(200);
-        s2.setPantPris(50);
-        s3.setPantPris(150);
+        s2.setPantPris(200);
+        s3.setPantPris(200);
+        s4.setPantPris(200);
+        s5.setPantPris(200);
         //Act
         double actualResult = Controller.samletPantPris();
 
         //Assert
-        assertEquals(400, actualResult);
+        assertEquals(1000, actualResult);
     }
 
     @Test
     void samletPantPris3() {
         //Arrange
-        Produkt s1 = Controller.createProdukt("bakki");
-        Produkt s2 = Controller.createProdukt("bakki");
-        Produkt s3 = Controller.createProdukt("bakki");
+        Produkt s1 = Controller.createProdukt("Fustage");
+        Produkt s2 = Controller.createProdukt("Fustage");
+        Produkt s3 = Controller.createProdukt("Fustage");
+        Produkt s4 = Controller.createProdukt("Fustage");
+        Produkt s5 = Controller.createProdukt("Fustage");
+        Produkt s6 = Controller.createProdukt("Fustage");
         s1.setPantPris(200);
-        s2.setPantPris(10);
-        s3.setPantPris(250);
+        s2.setPantPris(200);
+        s3.setPantPris(200);
+        s4.setPantPris(200);
+        s5.setPantPris(200);
+        s6.setPantPris(200);
 
         //Act
         double actualResult = Controller.samletPantPris();
 
         //Assert
-        assertEquals(460, actualResult);
+        assertEquals(1200, actualResult);
     }
+
+    @Test
+    void samletPantPris4() {
+        //Arrange
+        Produkt s1 = Controller.createProdukt("Fustage");
+        Produkt s2 = Controller.createProdukt("Fustage");
+        Produkt s3 = Controller.createProdukt("Fustage");
+        Produkt s4 = Controller.createProdukt("Fustage");
+        Produkt s5 = Controller.createProdukt("Fustage");
+        Produkt s6 = Controller.createProdukt("Fustage");
+        Produkt s7 = Controller.createProdukt("Fustage");
+        Produkt s8 = Controller.createProdukt("Fustage");
+        Produkt s9 = Controller.createProdukt("Fustage");
+        Produkt s10 = Controller.createProdukt("Fustage");
+        s1.setPantPris(200);
+        s2.setPantPris(200);
+        s3.setPantPris(200);
+        s4.setPantPris(200);
+        s5.setPantPris(200);
+        s6.setPantPris(200);
+        s7.setPantPris(200);
+        s8.setPantPris(200);
+        s9.setPantPris(200);
+        s10.setPantPris(200);
+
+        //Act
+        double actualResult = Controller.samletPantPris();
+
+        //Assert
+        assertEquals(2000, actualResult);
+    }
+
 
 }
